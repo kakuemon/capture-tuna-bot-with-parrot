5b57b1e84d11ff32c6a2db67299e697aecd56150
TomohiroKakuta <jobkakuta2019@gmail.com>
2020-06-05 22:02:01 +0900

    first

---
+++ main.py
@@ -0,0 +1,105 @@
+from flask import Flask, request
+import random, json, requests
+import pandas as pd
+
+# line用ライブラリ
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage,ImageSendMessage
+)
+
+import os
+# conunt maguro 
+maguro_count=0
+
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+# LINE APIおよびWebhookの接続s
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+def maguro_image_message():
+    messages = ImageSendMessage(
+        original_content_url="https://linebot-20200604-first.df.r.appspot.com/images/maguro.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        preview_image_url="https://linebot-20200604-first.df.r.appspot.com/images/maguro_mini.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+    )
+    return messages
+
+def neta_image_message():
+    messages = ImageSendMessage(
+        original_content_url="https://linebot-20200604-first.df.r.appspot.com/images/sushi.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        preview_image_url="https://linebot-20200604-first.df.r.appspot.com/images/sushi.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+    )
+    return messages
+
+# Flaskのルート設定
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# メッセージ応答メソッド
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    #　メッセージは "event.message.text" という変数に格納される
+    if event.message.text == "おはよう":
+        text = "おはようございます"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=text)
+        )
+    elif event.message.text == "スタンプ":
+        line_bot_api.reply_message(
+            event.reply_token,
+            StickerSendMessage(package_id=1 ,sticker_id=1)
+        )
+    elif event.message.text == "マグロ":
+        global maguro_count
+        maguro_count += 1
+        if maguro_count == 5:
+            maguro_count = 0
+            messages = neta_image_message()
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages)
+        else:
+            messages = maguro_image_message()
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages)
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
